146bb49b06636b9d818b0e07af973073d94a0953
Andrew Chen <54372234+dishearten@users.noreply.github.com>
2020-06-02 17:05:38 -0400

    ccc12j2

--- Canadian Computing Contest/ccc12j2.cpp
+++ Canadian Computing Contest/ccc12j2.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char arr [7] = {'I', 'O', 'S', 'H', 'Z','X','N'};
+    string s; cin >> s;
+    for (int i = 0; i < s.length(); i++){
+        bool contained = false;
+        for (int j = 0; j < 7; j++){
+            if (arr[j] == s[i]){
+                contained = true;
+            }
+        }
+        if (!contained){
+            cout << "NO";
+            exit(0);
+        }
+    }
+    cout << "YES";
     return 0;
 }
