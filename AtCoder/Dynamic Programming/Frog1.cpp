8d0c2819c15f3c4fb5ca2d28ade18dabff72cf86
Andrew Chen <54372234+dishearten@users.noreply.github.com>
2020-06-05 11:13:24 -0400

    fixed frog 1

--- AtCoder/Dynamic Programming/Frog1.cpp
+++ AtCoder/Dynamic Programming/Frog1.cpp
@@ -11,7 +11,7 @@ int main() {
     }
     vector<int> minimum (n);
     minimum[0] = 0;
-    minimum[1] = h[1] - h[0];
+    minimum[1] = abs(h[1] - h[0]);
     for (int i = 2; i < n; i++){
         minimum[i] = min(minimum[i - 2] + abs(h[i] - h[i - 2]), minimum[i - 1] + abs(h[i] - h[i - 1]));
     }
